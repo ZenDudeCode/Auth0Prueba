086d829a27869224c3581f2d52812a8280399f7b
Adrian Salas <aesainvest1510@gmail.com>
2020-07-22 10:36:04 -0400

    arreglado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { AuthService } from './services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  constructor(private auth: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-    //this.auth.localAuthSetup();
+
   }
 }
